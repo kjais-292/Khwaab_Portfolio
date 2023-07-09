dd27583e0bc923419dfadc6494bef5765cb2a2d9
Khwaab Jaiswal <khwaabjaiswal2902@gmail.com>
2023-07-09 14:04:47 +0530

    Deployed website

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
   );
   root.render(
-    <React.StrictMode>
+    
   <App/>
-    </React.StrictMode>
+    
     );
-    reportWebVitals();
